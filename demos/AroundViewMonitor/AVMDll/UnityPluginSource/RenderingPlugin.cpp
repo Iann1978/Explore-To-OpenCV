fd862a3090d5cc8a3ecf0d5271ccc2b0a68ddf8f
Iann Dang <iann1978@163.com>
2018-11-29 09:51:24 +0800

    - clean.

--- demos/AroundViewMonitor/AVMDll/UnityPluginSource/RenderingPlugin.cpp
+++ demos/AroundViewMonitor/AVMDll/UnityPluginSource/RenderingPlugin.cpp
@@ -79,22 +79,8 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 	{
 		
 		GLenum err;
-		//if (!image)
-		//{
-		//	image = new char[2048 * 2048 * 4];
-		//	//glGenTextures(1, &tempTexture);
-		//	err = glGetError();
-		//}
 
-
-
-		//if (!outputs)
-		//{
-		//	outputs = new Mat*[16];
-		//	memset(outputs, sizeof(Mat*) * 16, 0);
-		//}
-
-
+		// Convert CVTexture to cv::Mat
 		for (int i = 0; i < 16; i++)
 		{
 			if (Texture::cvTextures[i])
@@ -117,30 +103,14 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 			}
 		}
 	
-		//
-		//glBindTexture(GL_TEXTURE_2D, Texture::cvTextures[0]->texture);
-		//err = glGetError();
-		//glGetTexImage(GL_TEXTURE_2D, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-		//err = glGetError();
 
-
-
-		
-		//outputs[0] = new Mat();
-		
+		// Process cv::Mat
 		OpenCVProcess opencvProcess;
 		opencvProcess.Process(nullptr, nullptr);
 
-		//glBindTexture(GL_TEXTURE_2D, Texture::cvTextures[16]->texture);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		//glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB8, 512, 256, 0, GL_BGRA, GL_UNSIGNED_BYTE, image);
 
-		//glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 512, 256, GL_RGB, GL_UNSIGNED_BYTE, image);
-		//glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, 512, 512, GL_RGB, GL_UNSIGNED_BYTE, outputs[0]->ptr());
 
+		// Convert cv::Mat to CVTexture
 		for (int i = 16; i < 32; i++)
 		{
 			if (Texture::cvTextures[i])
@@ -151,9 +121,7 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 		}
 
 
-		err = glGetError();
-
-
+		// Release resources
 		for (int i = 0; i < 32; i++)
 		{
 			if (Texture::cvTextures[i])
@@ -165,20 +133,6 @@ static void UNITY_INTERFACE_API OnRenderEvent(int eventID)
 				}
 			}
 		}
-
-		//delete inputs[0];
-		//inputs[0] = nullptr;
-		//delete outputs[0];
-		//outputs[0] = nullptr;
-		//Mat sourceImage = Mat(256, 512, CV_8UC3, image);
-		//Mat greyImage;
-
-		//cvtColor(sourceImage, greyImage, COLOR_RGB2GRAY);
-
-		//Matrix = Mat(texture_height, texture_width, CV_8UC3, texture_bytes);
-
-
-		//glGetTexImage(Texture::cvTextures[1]->texture, 0, GL_RGBA, GL_SRGB8_ALPHA8, image);
 
 		//RenderDevice::ins->SetRenderTarget(Texture::cvTextures[16]->texture);
 		////RenderDevice::ins->DrawTestTriangle();
